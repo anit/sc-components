0edf2140c4bd41b4f65d00d4e40ac478ce4ba44f
Madhusudhan Srinivasa <madhums8@gmail.com>
2014-11-26 22:45:22 +0100

    Release 0.1.33

--- dist/sc-components.js
+++ dist/sc-components.js
@@ -1,7 +1,7 @@
 /**
  * sc-components
  * Simple reusable angular UI components
- * @version 0.1.32
+ * @version 0.1.33
  * Copyright(c) SafetyChanger
  * @license MIT
  */
@@ -771,83 +771,85 @@ angular.module('sc-list', [])
 
 angular.module('sc-listing', [])
 
-.directive('scListing', function ($compile, $http, $q, $templateCache) {
-  return {
-    restrict: 'E',
-    link: function ($scope, element, attrs) {
-      var scope = $scope.$new();
-      var isDefined = angular.isDefined;
-      var deferred = $q.defer();
-      var promise = deferred.promise;
-      var classes = ['list sc-list'];
-      var itemClass = ['list-item sc-list-item'];
-      var template;
-      var templateUrl;
+.directive('scListing', [
+  '$compile', '$http', '$q', '$templateCache', '$location',
+  function ($compile, $http, $q, $templateCache, $location) {
+    return {
+      restrict: 'E',
+      link: function ($scope, element, attrs) {
+        var scope = $scope.$new();
+        var isDefined = angular.isDefined;
+        var deferred = $q.defer();
+        var promise = deferred.promise;
+        var classes = ['list sc-list'];
+        var itemClass = ['list-item sc-list-item'];
+        var template;
+        var templateUrl;
 
-      // Parse attrs
+        // Parse attrs
 
-      // on-item-click
-      if (isDefined(attrs.onItemClick)) {
-        scope.onItemClick = $scope.$eval(attrs.onItemClick);
+        // on-item-click
+        if (isDefined(attrs.onItemClick)) {
+          scope.onItemClick = $scope.$eval(attrs.onItemClick);
+        }
+
+        // template and template-url
+        if (isDefined(attrs.template)) {
+          template = $scope.$eval(attrs.template);
+          deferred.resolve(template);
+        } else if (isDefined(attrs.templateUrl)) {
+          templateUrl = $scope.$eval(attrs.templateUrl);
+          $http.get(templateUrl, { cache: $templateCache })
+            .success(function (html) {
+              deferred.resolve(html);
+            })
+            .error(deferred.reject);
+        } else {
+          deferred.resolve('');
+        }
+
+        // class
+        if (isDefined(attrs.class)) {
+          classes.push(attrs.class);
+        }
+
+        // item-class
+        if (isDefined(attrs.itemClass)) {
+          itemClass.push($scope.$eval(attrs.itemClass));
+        }
+
+        // active
+        scope.active = isDefined(attrs.active)
+          ? $scope.$eval(attrs.active)
+          : angular.noop;
+
+        $scope.$watch(attrs['items'], function (items) {
+          scope.items = items;
+        });
+
+        $scope.$watch(attrs['ngModel'], function (ngModel) {
+          scope.ngModel = ngModel;
+        });
+
+        classes = classes.join(' ');
+        itemClass = itemClass.join(' ');
+
+        promise.then(function (tpl) {
+          tpl = tpl || '{{ item | json }}';
+
+          var template = [
+            '<ul class="'+ classes +'">',
+            '  <li class="'+ itemClass +'" ng-repeat="item in items" ng-click="onItemClick(item, $index)" ng-class="{ \'active\': active(item), \'last\': $last, \'first\': $first }" }">',
+            '    '+ tpl,
+            '  </li>',
+            '</ul>'
+          ].join('');
+
+          element.replaceWith($compile(template)(scope));
+        });
       }
-
-      // template and template-url
-      if (isDefined(attrs.template)) {
-        template = $scope.$eval(attrs.template);
-        deferred.resolve(template);
-      } else if (isDefined(attrs.templateUrl)) {
-        templateUrl = $scope.$eval(attrs.templateUrl);
-        $http.get(templateUrl, { cache: $templateCache })
-          .success(function (html) {
-            deferred.resolve(html);
-          })
-          .error(deferred.reject);
-      } else {
-        deferred.resolve('');
-      }
-
-      // class
-      if (isDefined(attrs.class)) {
-        classes.push(attrs.class);
-      }
-
-      // item-class
-      if (isDefined(attrs.itemClass)) {
-        itemClass.push($scope.$eval(attrs.itemClass));
-      }
-
-      // active
-      scope.active = isDefined(attrs.active)
-        ? $scope.$eval(attrs.active)
-        : angular.noop;
-
-      $scope.$watch(attrs['items'], function (items) {
-        scope.items = items;
-      });
-
-      $scope.$watch(attrs['ngModel'], function (ngModel) {
-        scope.ngModel = ngModel;
-      });
-
-      classes = classes.join(' ');
-      itemClass = itemClass.join(' ');
-
-      promise.then(function (tpl) {
-        tpl = tpl || '{{ item | json }}';
-
-        var template = [
-          '<ul class="'+ classes +'">',
-          '  <li class="'+ itemClass +'" ng-repeat="item in items" ng-click="onItemClick(item, $index)" ng-class="{ \'active\': active(item), \'last\': $last, \'first\': $first }" }">',
-          '    '+ tpl,
-          '  </li>',
-          '</ul>'
-        ].join('');
-
-        element.replaceWith($compile(template)(scope));
-      });
-    }
-  };
-});
+    };
+}]);
 
 // TODO: give a provider to override defaults
 
