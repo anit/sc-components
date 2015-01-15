2b89ff84e595be4c7bd30a124dd92a9f926d2abf
anit <anit.rai@changer.nl>
2015-01-15 10:52:10 +0530

    bump the version no. to 0.1.37

--- dist/sc-components.js
+++ dist/sc-components.js
@@ -1,7 +1,7 @@
 /**
  * sc-components
  * Simple reusable angular UI components
- * @version 0.1.36
+ * @version 0.1.37
  * Copyright(c) SafetyChanger
  * @license MIT
  */
@@ -729,6 +729,22 @@ angular.module('sc-list', [])
     this.options.page = parseInt(page, 10);
     return this.fetch();
   };
+  
+  /**
+   * load more
+   *
+   * @return {Array}
+   * @api public
+   */
+  List.prototype.more = function () {
+    var pageNo = this.items && this.items.length ?
+        this.options.page + 1 :
+        this.options.page;
+
+    return this.fetch({ 
+      page: pageNo
+    }, true);
+  };
 
   /**
    * fetch
@@ -739,24 +755,26 @@ angular.module('sc-list', [])
    */
 
   List.prototype.refresh =
-  List.prototype.fetch = function (options) {
+  List.prototype.fetch = function (options, append) {
     var self = this;
     options = options || {};
-    options.limit = options.limit || this.options.limit;
-    options.page = options.page || this.options.page;
-    options.sort_by = options.sort_by || this.options.sort_by;
-    options.sort_type = options.sort_type || this.options.sort_type;
 
     angular.extend(this.options, options);
     var items = this.Resource.query(this.options, function (res, headers) {
       self.headers = headers();
     });
+
     if (!this.items) this.items = [];
     this.items['$promise'] = items.$promise;
     this.$promise = items.$promise;
     items.$promise.then(function () {
-      self.items = items;
+      if (append) {
+        self.items.push.apply(self.items, items);
+      } else {
+        self.items = items;
+      }
     });
+
     return items;
   };
 
